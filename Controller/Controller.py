d80c080e09090703c8bd0b30c725b4d75298eeec
caleosilva <caleosilva75@gmail.com>
2023-08-22 21:58:29 -0300

    feat: adicionando bloqueio no Client

--- Controller/Controller.py
+++ Controller/Controller.py
@@ -12,62 +12,69 @@ from config import server_host, socket_host, socket_port
 import json
 
 
+# {client_socket: True, client_socket: True, client_socket: False}
+conexoes = {}
+
 class ResponseData:
     def __init__(self, id, response, client_socket):
         self.id = id
         self.response = response
         self.client_socket = client_socket
 
-def accept_connections(server_socket, data_queue):
+def aceitar_conexoes(server_socket, data_queue):
     while True:
         client_socket, client_address = server_socket.accept()
+        conexoes[client_socket] = True
+
         print("Nova conexão:", client_address, "\n")
+        # print("conexoes:", conexoes, "\n")
+
         client_thread = threading.Thread(target=handle_client, args=(client_socket, data_queue))
         client_thread.start()
 
 def handle_client(client_socket, data_queue):
-    while True:  # Mantém o socket aberto para receber novos IDs continuamente
-        try:
-            id = client_socket.recv(1024).decode('utf-8')
-            if id:
-                data_queue.append((id, client_socket))
-        except Exception as e:
-            print("Erro ao lidar com o cliente:", e)
-            break  # Encerra o loop se ocorrer um erroI
+    try:
 
-def make_http_requests(data_queue, response_queue):
+        while True:  # Mantém o socket aberto para receber novos IDs continuamente
+            id = client_socket.recv(1024).decode('utf-8')
+            if not id:  # Se o cliente fechou a conexão
+                print("Conexão fechada pelo cliente:", client_socket.getpeername())
+                break  # Sair do loop
+            data_queue.append((id, client_socket))
+            
+    except Exception as e:
+        print("Erro ao lidar com o cliente:", e)
+    finally:
+        if client_socket in conexoes:
+            del conexoes[client_socket]  # Remover a conexão do dicionário
+        client_socket.close()  # Fechar o socket
+
+
+def realizar_requisicoes_http(data_queue):
     while True:
         
         if data_queue:
             id, client_socket = data_queue.popleft()
             try:
-                # print("make_http_requests: ", client_socket, "\n")
-                response = requests.get(server_host + id)
-                response_queue.append(ResponseData(id, response, client_socket))
+
+                if not conexoes.get(client_socket):
+                    client_socket.send("Caixa bloqueado".encode('utf-8'))
+                else:
+                    response = requests.get(server_host + id)
+
+                    if (response.status_code == 204):
+                        mensagem = "Produto não encontrado"
+                        client_socket.send(mensagem.encode('utf-8'))
+
+                    elif (response.status_code == 200):                
+                        data_dict = response.json()
+                        data = json.dumps(data_dict)
+                        client_socket.send(data.encode('utf-8'))
+
+                    else:
+                        client_socket.send("Erro".encode('utf-8'))
             except Exception as e:
                 print("Erro ao fazer a solicitação HTTP:", e)
-
-def process_http_responses(response_queue):
-    while True:
-        if response_queue:
-            response_data = response_queue.popleft()
-            try:
-
-                if (response_data.response.status_code == 204):
-                    mensagem = "Produto não encontrado"
-                    response_data.client_socket.send(mensagem.encode('utf-8'))
-
-                elif (response_data.response.status_code == 200):                
-                    data_dict = response_data.response.json()
-                    data = json.dumps(data_dict)
-                    response_data.client_socket.send(data.encode('utf-8'))
-
-                else:
-                    response_data.client_socket.send("Erro".encode('utf-8'))
-
-
-            except Exception as e:
-                print("Erro ao enviar resposta para o cliente:", e)
 
 def main():
     host = socket_host
@@ -79,16 +86,12 @@ def main():
     print("Já estou ouvindo o socket em", host, "na", port)
 
     data_queue = deque()
-    response_queue = deque()
 
-    accept_thread = threading.Thread(target=accept_connections, args=(server_socket, data_queue))
+    accept_thread = threading.Thread(target=aceitar_conexoes, args=(server_socket, data_queue))
     accept_thread.start()
 
-    http_request_thread = threading.Thread(target=make_http_requests, args=(data_queue, response_queue))
+    http_request_thread = threading.Thread(target=realizar_requisicoes_http, args=(data_queue,))
     http_request_thread.start()
-
-    response_process_thread = threading.Thread(target=process_http_responses, args=(response_queue,))
-    response_process_thread.start()
 
 if __name__ == "__main__":
     main()
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 socket_host = '127.0.0.1' # Host
-socket_port = 3431
+socket_port = 3433
 server_host = 'http://localhost:8000/'
